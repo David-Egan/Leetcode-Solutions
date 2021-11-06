0bc8efb51044b2783fd7ab7bf81908a7f6c12365
David Egan <davideganse@gmail.com>
2021-11-06 04:23:52 -0400

    Update de-cpp-1480.cpp

--- solutions/1480_Running-Sum-of-1d-Array/de-cpp-1480.cpp
+++ solutions/1480_Running-Sum-of-1d-Array/de-cpp-1480.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public: 
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> result;
-        int lastResult = 0;
-        
-        
-        for(int i = 0; i < nums.size(); i++){
-            
-            result.push_back(lastResult + nums[i]);
-            lastResult = result[i];
+        for(int i = 1; i < nums.size(); i++){
+            nums[i] = nums[i-1] + nums[i];
         }
         
-        return result;
+        return nums;
     }
 };
